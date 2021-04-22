3b1103b98835b8fb913e918a7bbb116b467542d9
Dmitry <dmitry.balduev@gmail.com>
2021-04-21 22:20:42 -0400

    added code that write cards with members on html

--- index.js
+++ index.js
@@ -114,25 +114,58 @@ function addIntern() {
 };
 
 function renderTeam (){
-    console.log(teamArr) ;
+    console.log(teamArr);
+    let cards = getHtmlStarter();
     for(let i = 0; i < teamArr.length; i++){
-        let cardText = `<div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-      </div>`;
-        if(teamArr[i] instanceof Manager){
-            console.log(teamArr[i] + " is type of Manager");
-            cardText += `<h5 class="card-title">${teamArr[i].name}</h5>
-            <h5 class="card-title">Manager</h5>
-            <p class="card-text">ID: ${teamArr[i].id}</p>
-            <p class="card-text">Email: ${teamArr[i].email}</p>
-            <p class="card-text">Office Number: ${teamArr[i].officeNumber}</p>`
-        } else if(teamArr[i] instanceof Intern){
-            console.log(teamArr[i] + " is type of Intern");
-        } else{
-            console.log(teamArr[i] + " is type of Engineer");
-        }
+       cards += generateCardText(teamArr[i]);
     }
+    cards += `    </body>
+    </html>`
+    fs.writeFile('./src/index.html', `${cards}\n`, (err) =>
+        err ? console.error(err) : console.log('Successfully created file!'));
+}
+
+function generateCardText(member){
+    let cardText = `<div class="card" style="width: 18rem;">
+    <div class="card-body">`
+
+    if(member instanceof Manager){
+        console.log(member + " is type of Manager");
+        cardText += `<h5 class="card-title">${member.name}</h5>
+        <h5 class="card-title">Manager</h5>
+        <p class="card-text">ID: ${member.id}</p>
+        <p class="card-text">Email: ${member.email}</p>
+        <p class="card-text">Office Number: ${member.officeNumber}</p>`
+    } else if(member instanceof Intern){
+        console.log(member + " is type of Intern");
+        cardText += `<h5 class="card-title">${member.name}</h5>
+        <h5 class="card-title">Intern</h5>
+        <p class="card-text">ID: ${member.id}</p>
+        <p class="card-text">Email: ${member.email}</p>
+        <p class="card-text">School: ${member.school}</p>`
+    } else{
+        console.log(member + " is type of Engineer");
+        cardText += `<h5 class="card-title">${member.name}</h5>
+        <h5 class="card-title">Engineer</h5>
+        <p class="card-text">ID: ${member.id}</p>
+        <p class="card-text">Email: ${member.email}</p>
+        <p class="card-text">Github: ${member.github}</p>`
+    }
+
+    cardText += `</div>
+    </div>`;
+
+    return cardText;
+}
+
+function getHtmlStarter(){
+    return `<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8" />
+            <title>Team Profile</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        </head>
+        <body>
+            <h1 class="justify-content-center"> <span class="badge bg-secondary">Team Profile</span></h1>`
 }
